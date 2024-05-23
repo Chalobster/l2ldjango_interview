7dc00f6064876607ba90647ca83e45de157bdcc5
chadlarson <chad@ncompass.org>
2024-05-22 21:27:37 -0600

    convert string date instead of return input

--- l2l/templatetags/l2l_customtags.py
+++ l2l/templatetags/l2l_customtags.py
@@ -1,15 +1,17 @@
+from datetime import datetime
 from django import template
 
 register = template.Library()
 
 @register.filter
 def l2l_dt(date_to_convert):
-    result = ""
+    result = "incorrect data type"
     if date_to_convert is not None:
         if type(date_to_convert) is str:
             #"%Y-%m-%dT%H:%M:%S"
-            result = date_to_convert
-        else:
+            result = dt = datetime.strptime(date_to_convert, "%Y-%m-%dT%H:%M:%S")
+            result = dt.strftime("%Y-%m-%dT%H:%M:%S")
+        elif type(date_to_convert) is datetime:
             #"%Y-%m-%d %H:%M:%S"
             result = date_to_convert.strftime("%Y-%m-%d %H:%M:%S")
 
